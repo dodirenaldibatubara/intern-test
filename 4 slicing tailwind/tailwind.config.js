c5cb58e5c452c492babb3d14007ca24df0a517dc
dodirenaldibatubara <dodirenaldi3@gmail.com>
2022-12-30 10:17:57 +0700

    update: add folder 4

---
+++ 4 slicing tailwind/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F66F4D",
+        secondary: "#FAF8ED",
+        third: "#2D3134",
+        fourth: "#FFD482",
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
